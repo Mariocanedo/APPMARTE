2218b01228fe74f3b84ab1c5655b12639317bd4c
StudioOne <Mario_cane.do@hotmail.com>
2022-12-26 21:31:46 -0300

    Falta Completar El adapter e implementarlo en la Vista

--- app/src/main/java/com/example/appmarte/AdapterMars.kt
+++ app/src/main/java/com/example/appmarte/AdapterMars.kt
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.appmarte
 
 import android.view.View
@@ -27,10 +28,55 @@ class AdapterMars  : RecyclerView.Adapter<AdapterMars.MarsVh>() {
         override fun onClick(v: View?) {
             // seleccionar un elemento a futuro
 
+=======
+package com.example.retrofitexample_0
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.appmarte.Model.Remote.MarsRealState
+import com.example.appmarte.databinding.MarsItemBinding
+
+class AdapterMars  : RecyclerView.Adapter<AdapterMars.MarsVH>() {
+
+
+    private var listMarsItem = listOf<MarsRealState>()
+    val selectedTerrain = MutableLiveData<MarsRealState>()
+
+    // Funcion  para seleccionar agregar
+
+
+
+    fun update(list: List<MarsRealState>) {
+        listMarsItem = list
+        notifyDataSetChanged()
+    }
+
+
+    //1 INNER CLASS
+
+    inner class MarsVH(private val binding: MarsItemBinding) :
+        RecyclerView.ViewHolder(binding.root),
+        View.OnClickListener {
+        fun bin(mars: MarsRealState) {
+            // ACA glide no olvidar dependencias
+            Glide.with(binding.imageView2).load(mars.img_Src).centerCrop().into(binding.imageView2)
+            // Activar el Clik
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+          // falta implementar al seleccionar un elemento
+>>>>>>> ec75243 (Falta Completar El adapter e implementarlo en la Vista)
         }
 
     }
 
+<<<<<<< HEAD
     // infla la vista de los elementos del layout mars_item
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsVh {
         TODO("Not yet implemented")
@@ -49,3 +95,22 @@ class AdapterMars  : RecyclerView.Adapter<AdapterMars.MarsVh>() {
     }
 
 }
+=======
+
+    //_______________________________Estos Métodos se implementan----------------------------
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsVH {
+        return MarsVH(MarsItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: MarsVH, position: Int) {
+        val terrain = listMarsItem[position]
+        holder.bin(terrain)
+    }
+
+    override fun getItemCount(): Int =
+        listMarsItem.size
+}
+
+
+>>>>>>> ec75243 (Falta Completar El adapter e implementarlo en la Vista)
